0ae614d84fc11ffbe98a4fe109e41e8f202f77c9
Lin Yi <professionally10@gmail.com>
2025-07-28 09:58:37 -0400

    install cors for server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var path = require('path');
 var { requestLogger } = require('./utils/logger');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var stocksRouter = require('./routes/stocks');
 
 var app = express();
+
+// Allow only the Vite dev server origin
+const allowedOrigins = ['http://localhost:5173'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 // Log every request (method, url, timestamp)
 app.use(requestLogger);
